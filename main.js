d86a2afb3e35d2870950503917278b01c0e9fbb0
Lucas L <127303072+lucaslongoo@users.noreply.github.com>
2023-11-08 09:56:27 -0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-    function tocaSomPom () {
-        document.querySelector('#som_tecla_tom').play();
-    }
-
-const ListaDeTeclas = document.querySelector('.tecla');
-
-ListaDeTeclas[0].onclick = tocaSomPom;
